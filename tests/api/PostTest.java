14543ce7c714b90d4fb96220364d522153312677
jimnt <jimntantakas@gmail.com>
2023-09-22 23:58:49 +0300

    Added tests for Login and Post classed

---
+++ tests/api/PostTest.java
@@ -0,0 +1,35 @@
+package api;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostTest {
+
+    @BeforeEach
+    void setUp() {
+        Content.nextID=0;
+        Comment.nextID=0;
+    }
+
+    @AfterEach
+    void tearDown() {
+        ControlSystem.comments=null;
+        ControlSystem.content=null;
+        //ControlSystem.liked=null;
+    }
+    @Test
+    void getText() {
+        Post post=new Post("My title","This is my first post","Jim");
+        assertEquals(post.getText(),"This is my first post");
+    }
+
+    @Test
+    void setText() {
+        Post post=new Post("My title","This is my first post","Jim");
+        post.setText("My new edited text");
+        assertEquals(post.getText(),"My new edited text");
+    }
+}
